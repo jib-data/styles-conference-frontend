f11e3eb3bcdf56809366d49d42791d266bea035c
Jibril Richter <jibrilrichter@gmail.com>
2024-10-06 21:56:44 -0500

    build navigation and hero section

---
+++ src/main/java/com/jibril/richter/App.java
@@ -0,0 +1,13 @@
+package com.jibril.richter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
